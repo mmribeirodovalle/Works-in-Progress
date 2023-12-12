e4453d762fb24902ce8575614f9eef7b79868890
mmribeirodovalle <145507526+mmribeirodovalle@users.noreply.github.com>
2023-12-12 15:03:13 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+let sliderElement= document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&^*()1234567890-_=+'><.,"
+let novaSenha = "";
+sizePassword.innerHTML=sliderElement.value;
+sliderElement.oninput = function (){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    let pass = "";
+    for(let i = 0, n = charset.length; i<sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+}
+
+console.log(pass)
+containerPassword.classList.remove("hide");
+password.innerHTML = pass;
+novaSenha=pass;
+}
+
+function copyPassword() {
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+}
